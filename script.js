12499b6b52ebb26a61e0d532d25d0d9544e48200
AryanRajdev <91838247+AryanRajdev@users.noreply.github.com>
2025-02-24 12:47:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+
+function daysOfAYear(year) {
+	if(year%4 == 0){
+		if(year%100 == 0){
+			if(year%400 == 0){
+				console.log(366);
+			}
+			else{
+				console.log(365);
+			}
+		}
+		else{
+			console.log(366);
+		}
+	}
+	else{
+		console.log(365);
+	}
+}
+
+let year = parseInt(prompt("Enter a year:"));
+daysOfAYear(year);
